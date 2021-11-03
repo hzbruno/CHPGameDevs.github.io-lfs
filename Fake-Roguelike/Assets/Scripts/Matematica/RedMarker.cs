e60dad890d3b55c1745e4498e6523b89d60237ee
Bruno <hzzbruno@gmail.com>
2021-11-03 17:42:25 -0300

    v1.0
    Ordenado Scripts-Assets

---
+++ Fake-Roguelike/Assets/Scripts/Matematica/RedMarker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedMarker : MonoBehaviour
+{
+    private float time = 0;
+    public GameObject laser;
+
+    void Start()
+    {
+        Instantiate(laser, new Vector3(-1.2f, gameObject.transform.position.y, 0), gameObject.transform.rotation);
+    }
+
+    void FixedUpdate()
+    {
+        time += Time.deltaTime;
+        if (time > 1)
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+        if (time > 2)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
